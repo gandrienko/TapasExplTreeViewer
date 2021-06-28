30d3883934d0010172b8e064d2a1a8163ae1a590
gandri <gennadiy.andriyenko@iais.fraunhofer.de>
2021-06-28 17:54:40 +0200

    dot transparency

--- src/TapasExplTreeViewer/vis/ExplanationsProjPlot2D.java
+++ src/TapasExplTreeViewer/vis/ExplanationsProjPlot2D.java
@@ -6,7 +6,7 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class ExplanationsProjPlot2D extends ProjectionPlot2D {
-  public static int minDotRadius=3, maxDotRadius=15;
+  public static int minDotRadius=4, maxDotRadius=20;
   public static float hsbRed[]=Color.RGBtoHSB(255,0,0,null);
   public static float hsbBlue[]=Color.RGBtoHSB(0,0,255,null);
   
@@ -20,6 +20,9 @@ public class ExplanationsProjPlot2D extends ProjectionPlot2D {
       for (int i = 0; i < explanations.size(); i++)
         if (maxNUses < explanations.get(i).nUses)
           maxNUses = explanations.get(i).nUses;
+    off_Valid=off_selected_Valid=false;
+    if (isShowing())
+      repaint();
   }
   
   public void drawPoint(Graphics2D g, int pIdx, int x, int y, boolean highlighted, boolean selected) {
@@ -34,6 +37,7 @@ public class ExplanationsProjPlot2D extends ProjectionPlot2D {
     if (!highlighted && !selected) {
       float actionRatio = (10 - ((float) ex.action)) / 10;
       color = Color.getHSBColor(actionRatio * (hsbBlue[0] - hsbRed[0]),1,1);
+      color=new Color(color.getRed(),color.getGreen(),color.getBlue(),100);
       g.setColor(color);
       g.fillOval(x-dotRadius-1,y-dotRadius-1,dotDiameter+2,dotDiameter+2);
     }

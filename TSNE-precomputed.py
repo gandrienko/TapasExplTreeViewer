2a8940b8e11d9da8633a6e7d87ee199539561de8
gandrienko <gennady.andrienko@iais.fraunhofer.de>
2021-07-05 18:12:18 +0200

    call Python for running TSNE

--- TSNE-precomputed.py
+++ TSNE-precomputed.py
@@ -10,22 +10,17 @@ import numpy as np
 import pandas as pd
 from sklearn import manifold
 
-#print(f"Arguments count: {len(sys.argv)}")
-#for i, arg in enumerate(sys.argv):
-#    print(f"Argument {i:>6}: {arg}")
-#print(sys.argv[0])
-#print(sys.argv[1])
-        
 fname='distances4tsne-1'
 if len(sys.argv)>1 and sys.argv[1] is not None:
     fname=sys.argv[1]
 A = np.genfromtxt(fname+'.csv',delimiter=',',dtype=None)
 
-perplexities = [30] #[5, 10, 20, 30, 40, 50, 75, 100]
+perplexity = 30
+if len(sys.argv)>2 and sys.argv[2] is not None:
+    perplexity=int(sys.argv[2])
 
-for i,perplexity in enumerate(perplexities):
-    model = manifold.TSNE(metric="precomputed", perplexity=perplexity)
-    Y = model.fit_transform(A) 
-    tsne_df = pd.DataFrame({'X':Y[:,0],'Y':Y[:,1]})
-    tsne_df.to_csv(fname+'_out_p'+str(perplexity)+'.csv')
+model = manifold.TSNE(metric="precomputed", perplexity=perplexity)
+Y = model.fit_transform(A)
+tsne_df = pd.DataFrame({'X':Y[:,0],'Y':Y[:,1]})
+tsne_df.to_csv(fname+'_out_p'+str(perplexity)+'.csv')
 
